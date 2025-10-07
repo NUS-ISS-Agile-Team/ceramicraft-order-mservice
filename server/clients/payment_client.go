d332863ed6eb4c993e7e2332e52f3e4723128a15
YangZhicheng <yangzhicheng@YangZhichengdeMacBook-Air.local>
2025-10-07 20:24:28 +0800

    fix: add log when init payment client

--- server/clients/payment_client.go
+++ server/clients/payment_client.go
@@ -3,8 +3,9 @@ package clients
 import (
 	"sync"
 
-	paymentClient "github.com/NUS-ISS-Agile-Team/ceramicraft-payment-mservice/client"
 	"github.com/NUS-ISS-Agile-Team/ceramicraft-order-mservice/server/config"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-order-mservice/server/log"
+	paymentClient "github.com/NUS-ISS-Agile-Team/ceramicraft-payment-mservice/client"
 	"github.com/NUS-ISS-Agile-Team/ceramicraft-payment-mservice/common/paymentpb"
 )
 
@@ -15,10 +16,15 @@ var (
 
 func InitPaymentClient(cfg *config.PaymentClient) {
 	paymentClientOnce.Do(func() {
-		paymentClientInstance, _ = paymentClient.GetPaymentClient(&paymentClient.GRpcClientConfig{
+		var err error
+		paymentClientInstance, err = paymentClient.GetPaymentClient(&paymentClient.GRpcClientConfig{
 			Host: cfg.Host,
 			Port: cfg.Port,
 		})
+		if err != nil {
+			log.Logger.Errorf("InitPaymentClient: init failed, err %s", err.Error())
+		}
+		log.Logger.Infoln("InitPaymentClient: success")
 	})
 }
 
